8ba4152ef984a16b84305deac9908800345b653f
sonchangwook <changw8k20@gmail.com>
2024-12-24 14:59:30 +0900

    path

---
+++ first-project/src/main/java/com/groupware_mini/first_project/config/RootConfiguration.java
@@ -0,0 +1,35 @@
+package com.groupware_mini.first_project.config;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = {"groupware_mini.first_project.mapper"})
+@EnableTransactionManagement
+public class RootConfiguration {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        // 트랜잭션 매니저 설정
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+
+}
